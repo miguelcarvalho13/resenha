6c689079c484eb069e0d1500b7dda2d7f50399dd
Miguel Carvalho <miguel.carvalho13@outlook.com>
2024-03-16 18:36:41 -0300

    chore: create t3 app
    Create default t3 app with its CLI and also add Dockerfile image and
    docker-compose.yml file to run it locally through Docker.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  output: "standalone",
+};
+
+export default config;
