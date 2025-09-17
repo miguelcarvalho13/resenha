9b5d6e2dad2d3e7831da7df9b7d7d524b4ea7cd5
Miguel Carvalho <miguel.carvalho13@outlook.com>
2025-09-17 14:35:07 -0300

    chore: add calls to signUp/In in the frontend
    This adds the methods provided by better-auth to perform sign up and
    sign in. Still not working in the backend though.

--- apps/web/src/components/auth/LoginAndSignUpForm.tsx
+++ apps/web/src/components/auth/LoginAndSignUpForm.tsx
@@ -1,14 +1,74 @@
+import { useNavigate } from "@tanstack/react-router";
 import { type FormEventHandler } from "react";
+import { z } from "zod";
+
+import { authClient } from "@/utils/authClient";
 
 interface LoginAndSignUpFormProps {
   mode: 'sign-up' | 'sign-in';
 }
 
+export const signUpSchema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export const signInSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
 export const LoginAndSignUpForm = ({ mode }: LoginAndSignUpFormProps) => {
+  const navigate = useNavigate({
+    from: "/",
+  });
   const isSignUp = mode === 'sign-up';
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const formDataValue = Object.fromEntries(formData.entries());
+
+    if (isSignUp) {
+      const values = signUpSchema.parse(formDataValue)
+
+      await authClient.signUp.email(
+        {
+          email: values.email,
+          password: values.password,
+          name: values.name,
+        },
+        {
+          onSuccess: () => {
+            console.log("Sign up successful");
+            navigate({ to: "/" });
+          },
+          onError: (ctx) => {
+            console.log(ctx.error.message);
+          },
+        },
+      );
+    } else {
+      const values = signInSchema.parse(formDataValue)
+
+      await authClient.signIn.email(
+        {
+          email: values.email,
+          password: values.password,
+        },
+        {
+          onSuccess: () => {
+            console.log("Sign in successful");
+            navigate({ to: "/" });
+          },
+          onError: (ctx) => {
+            console.log(ctx.error.message)
+          },
+        },
+      );
+    }
   }
 
   return (
