418c48e76db893aba6d1086a33f667f7834bc5f6
Miguel Carvalho <miguel.carvalho13@outlook.com>
2025-10-04 16:04:44 -0300

    fix(web): add type assert for trpc
    Not sure why this all of sudden needed, will probably need further
    investigation in the future.

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -2,4 +2,5 @@ import { createTRPCReact } from '@trpc/react-query';
 
 import type { AppRouter } from '@repo/api';
 
-export const trpc = createTRPCReact<AppRouter>();
+export const trpc: ReturnType<typeof createTRPCReact<AppRouter>> =
+  createTRPCReact<AppRouter>();
