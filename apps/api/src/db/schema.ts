cfb5b3bfd5a45f078bdaf77f405ff5d215646612
Miguel Carvalho <miguel.carvalho13@outlook.com>
2025-12-01 20:58:13 -0300

    chore(api): add type inference to searches content

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -13,6 +13,8 @@ import {
   uuid,
 } from 'drizzle-orm/pg-core';
 
+import { type SearchNotesSchemaType } from '@api/schemas/searches';
+
 // Tables
 export const users = pgTable('users', {
   id: text('id').primaryKey(),
@@ -159,7 +161,7 @@ export const noteTags = pgTable(
 
 export const searches = pgTable('searches', {
   id: uuid('id').defaultRandom().primaryKey(),
-  content: json('content').notNull(),
+  content: json('content').notNull().$type<SearchNotesSchemaType>(),
   name: text('name'),
   favorited: boolean().default(false).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
