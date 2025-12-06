7684b6caf68d1a928b62c5912b67432da9dc1927
Miguel Carvalho <miguel.carvalho13@outlook.com>
2025-12-06 11:30:13 -0300

    chore(web): add `cursor: inherit` to TagWrapper

--- apps/web/src/components/tag/TagWrapper.tsx
+++ apps/web/src/components/tag/TagWrapper.tsx
@@ -3,7 +3,7 @@ import { Badge, Group, type BadgeProps } from '@mantine/core';
 export interface TagWrapperProps extends BadgeProps {}
 
 export const TagWrapper = ({ children, ...props }: TagWrapperProps) => (
-  <Badge data-testid="tag" size="md" {...props}>
+  <Badge data-testid="tag" size="md" style={{ cursor: 'inherit' }} {...props}>
     <Group className="gap-0.5">{children}</Group>
   </Badge>
 );
